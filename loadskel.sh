3b0caf81685355b714a21e729217a891949456f0
Trevor Ellermann <trevor@ellermann.net>
2012-08-19 15:09:27 -0700

    Updated loadskel to be safer

--- loadskel.sh
+++ loadskel.sh
@@ -1,10 +1,34 @@
 #! /bin/bash
 
-cp -v skel/.bash_logout ~
-cp -v skel/.bash_profile ~
-cp -v skel/.bashrc ~
-cp -v skel/.forward ~
-cp -v skel/.gitconfig ~
-cp -v skel/.vimrc ~
+# make .backup to preserve existing skel files
+if [ ! -d ~/.backup ]; then
+    mkdir ~/.backup
+fi
 
-cp -v -Rp skel/.vim ~
+# make .vim .vim/backup and .vim/tmp if they don't exist
+if [ ! -d ~/.vim ]; then
+    mkdir ~/.vim
+fi
+
+if [ ! -d ~/.vim/backup ]; then
+    mkdir ~/.vim/backup
+fi
+
+if [ ! -d ~/.vim/tmp ]; then
+    mkdir ~/.vim/tmp
+fi
+
+TIME=$(/bin/date +%s)
+
+# Backup files to ~/.backup before you clobber them
+safe_copy() {
+  cp -p ~/$@ ~/.backup/$@-$TIME
+  cp -v skel/$@ ~
+}
+
+safe_copy ".bash_logout"
+safe_copy ".bash_profile"
+safe_copy ".bashrc"
+safe_copy ".forward"
+safe_copy ".gitconfig"
+safe_copy ".vimrc"
