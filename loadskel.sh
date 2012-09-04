cde17033b03860cc43bd480ab3d8c2d67ac330cb
Trevor Ellermann <trevor@ellermann.net>
2012-09-03 22:15:36 -0700

    Updated permissions

--- loadskel.sh
+++ loadskel.sh
@@ -1,19 +1,23 @@
 #! /bin/bash
 
+# Make sure the home dir permissions are restrictive
+chmod 700 ~
+
 # make .backup to preserve existing skel files
 if [ ! -d ~/.backup ]; then
     mkdir ~/.backup
 fi
+chmod 700 ~/.backup
 
 if [ ! -d ~/.backup/.ssh ]; then
     mkdir ~/.backup/.ssh
 fi
 
-
 # make .vim .vim/backup and .vim/tmp if they don't exist
 if [ ! -d ~/.vim ]; then
     mkdir ~/.vim
 fi
+chmod 700 ~/.vim
 
 if [ ! -d ~/.vim/backup ]; then
     mkdir ~/.vim/backup
@@ -26,6 +30,7 @@ fi
 if [ ! -d ~/.ssh ]; then
     mkdir ~/.ssh
 fi
+chmod 700 ~/.ssh
 
 TIME=$(/bin/date +%s)
 
@@ -35,8 +40,10 @@ safe_copy() {
     cp -p ~/$@ ~/.backup/$@-$TIME
   fi
   cp -v ~/git/boneyard/skel/$@ ~/$@
+  chmod 644 ~/$@
 }
 
+# Files to copy from skel
 safe_copy ".bash_logout"
 safe_copy ".bash_profile"
 safe_copy ".bashrc"
